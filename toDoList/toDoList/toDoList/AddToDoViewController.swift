024a101b0d01116d7f35fbad09418d837eec1877
shriya-rudraraju <108313725+shriya-rudraraju@users.noreply.github.com>
2022-08-01 11:43:11 -0400

    mid iteration 1 again

--- toDoList/toDoList/toDoList/AddToDoViewController.swift
+++ toDoList/toDoList/toDoList/AddToDoViewController.swift
@@ -8,14 +8,25 @@
 import UIKit
 
 class AddToDoViewController: UIViewController {
-
+    var previousToDoTVC = ToDoTableViewController()
+    @IBOutlet weak var descriptionInput: UITextField!
+    
+    @IBOutlet weak var switchInput: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        let newToDo = ToDoClass()
+        
+        if let checkForInput = descriptionInput.text {
+            newToDo.description = checkForInput
+            newToDo.important = switchInput.isOn
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
